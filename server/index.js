48a6a558b27c67622fb52137488339add3a0a6a6
Daniel Van Acker <danvan4477@gmail.com>
2020-04-11 19:05:31 -0400

    Added lead scientist and scientist endpoints

--- server/index.js
+++ server/index.js
@@ -44,10 +44,48 @@ const getCapableShips = (request, response) => {
   })
 }
 
+const getAvailScientists = (request, response) => {
+  const {e_name} = request.query;
+
+  pool.query('WITH s1 AS (SELECT eid FROM scientist WHERE eid NOT IN (SELECT eid FROM partakesin)),' +
+  `s2 AS (SELECT start_date, end_date FROM expedition WHERE e_name = \'${e_name}\'),` +
+  's3 AS (SELECT e_name FROM expedition JOIN s2 ON expedition.start_date > s2.end_date OR expedition.end_date < s2.start_date),' +
+  's4 AS (SELECT eid FROM partakesin JOIN s3 ON partakesin.e_name = s3.e_name),' +
+  's5 AS (SELECT * FROM s1 UNION SELECT * FROM s4)' +
+  'SELECT s5.eid, full_name FROM scientist JOIN s5 ON scientist.eid = s5.eid;', [], (error, results) => {
+    if(error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getAvailLeadScientists = (request, response) => {
+  const {e_name} = request.query;
+
+  pool.query('WITH s1 AS (SELECT eid, full_name FROM leadscientist WHERE eid NOT IN (SELECT lead_eid FROM expedition)),' +
+  `s2 AS (SELECT start_date, end_date FROM expedition WHERE e_name=\'${e_name}\'),` +
+  's3 AS (SELECT lead_eid FROM expedition JOIN s2 ON expedition.start_date > s2.end_date OR expedition.end_date < s2.start_date),' +
+  's4 AS (SELECT eid, full_name FROM leadscientist ls JOIN s3 ON ls.eid = s3.lead_eid)' +
+  'SELECT * FROM s4 UNION SELECT * FROM s1;', [], (error, results) => {
+    if(error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 const closePool = (request, response) => {
     pool.end();
     response.status(200).json({message: 'Connection closed.'})
 }
+
+app.route('/getAvailScientists')
+  .get(getAvailScientists)
+
+  app.route('/getAvailLeadScientists')
+  .get(getAvailLeadScientists)
+
 
 app.route('/getCapableShips')
   .get(getCapableShips)
